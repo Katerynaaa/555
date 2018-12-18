3c9b3f65292f92ef9ac5834079c5d452def9a78b
Katerynaaa <43213490+Katerynaaa@users.noreply.github.com>
2018-12-19 00:31:30 +0200

    Add files via upload

---
+++ main.java
@@ -0,0 +1,10 @@
+package lab5;
+
+public class main {
+    public static void main(String[] args) {
+        Text text = new Text("Виви попросила попкрон, а перпендикуляр кукорекал кококо.\nМы нормальные.");
+        text.Sent();
+        System.out.println(text);
+
+    }
+}
