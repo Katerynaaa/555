f097acf0c80d381aa4dbdcaee5ab8f876dc5cc28
Oleksii Aleshchenko <oleksii.aleshchenko@netcracker.com>
2018-12-19 12:04:04 +0200

    fix from teacher

---
+++ lab5/Text.java
@@ -0,0 +1,30 @@
+package lab5;
+
+public class Text {
+    private Sentence[] sentences;
+
+    public Text(String s) {
+        String[] split = s.split("\n");
+        sentences = new Sentence[split.length];
+        for (int i = 0; i < split.length; i++) {
+            sentences[i] = new Sentence(split[i]);
+        }
+    }
+    @Override
+    public String toString() {
+        String result = "";
+        for (Sentence sentence : sentences) {
+            result += sentence.toString() + "";
+        }
+        return result;
+    }
+    public void sent() {
+        {
+            for (Sentence k : sentences) {
+
+                    k.sentMemb();
+
+            }
+        }
+    }
+}
---
+++ lab5/main.java
@@ -0,0 +1,10 @@
+package lab5;
+
+public class main {
+    public static void main(String[] args) {
+        Text text = new Text("виви попросила попкрон, а перпендикуляр кукорекал кококо.\nмы нормальные.");
+        text.sent();
+        System.out.println(text);
+
+    }
+}
